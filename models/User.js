760dd35244f1364cb6b004cd22e4019469589010
msnaye <99111936+msnaye@users.noreply.github.com>
2022-07-18 02:15:39 -0500

    added index

--- models/User.js
+++ models/User.js
@@ -6,32 +6,30 @@ class User extends Model {}
 
 // define table columns and configuration
 User.init(
-  {
-    // TABLE COLUMN DEFINITIONS GO HERE
-    id:{
+    {
+      id: {
         type: DataTypes.INTEGER,
         allowNull: false,
         primaryKey: true,
         autoIncrement: true
-    },
-    username:{
-        type:DataTypes.STRING,
-        allowNull: false,
-
-    },
-    email:{
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true,
         validate: {
-            isEmail: true
+          isEmail: true
         }
-    },
-    password:{
+      },
+      password: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
-            len: [4]
+          len: [4]
         }
     }
 
